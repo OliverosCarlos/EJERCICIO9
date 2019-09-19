0d4345793bb76f1fbe2fd502a9eea746a83b778c
Carlos Moises Ruvalcaba Oliveros <camoruvalcabaol@ittepic.edu.mx>
2019-09-19 10:24:36 -0600

    app start

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u1_ejercicio9/MainActivity.kt
@@ -0,0 +1,45 @@
+package mx.edu.ittepic.tpdm_u1_ejercicio9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+
+    var txtN : EditText ?= null
+    var txtM : EditText ?= null
+    var n = 0
+    var m = 0
+    var btnCalcular : Button ?= null
+
+    var layTabla : LinearLayout ?= null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtN = findViewById(R.id.n)
+        txtM = findViewById(R.id.m)
+
+        btnCalcular = findViewById(R.id.btn)
+
+        layTabla = findViewById(R.id.layoutTabla)
+
+        btnCalcular?.setOnClickListener {
+            calcular()
+        }
+    }
+
+    fun calcular(){
+        n = txtN?.text.toString().toInt()
+        m = txtM?.text.toString().toInt()
+        Toast.makeText(this,"datos $n, $m",Toast.LENGTH_LONG).show()
+        (1..m).forEach {
+            var r = n*m
+            var etiqueta = TextView(this)
+            etiqueta.setText("$n x $it = $r")
+            layTabla?.addView(etiqueta)
+        }
+    }
+}
